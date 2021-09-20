62246036c57481ea62ba32c7bc0f59dc5cbd82ca
Chris Browet <cbro@semperpax.com>
2021-09-20 14:13:53 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,70 @@
+# MQTT DiscoveryStream integration for Home Assistant
+
+This is an "extension" of the builtin [`mqtt_statestream`](https://www.home-assistant.io/integrations/mqtt_statestream/) integration.  
+Besides the functionalities of the hereabove, it also allows to publish and handles an [MQTT "disvovery"](https://www.home-assistant.io/docs/mqtt/discovery) setup.
+
+## Changelog
+
+### 0.1
+
+- Initial release:
+  Handles:
+    - sensors
+    - switches
+    - lights (partial)
+
+## Pre-requistes
+
+1. MQTT configured
+
+## Installation
+
+### HACS
+
+1. Launch HACS
+1. Navigate to the Integrations section
+1. "+ Explore & Add Repositories" button in the bottom-right
+1. Search for "MQTT DiscoveryStream"
+1. Select "Install this repository"
+1. Restart Home Assistant
+
+### Home Assistant
+
+The integration is configured via YAML only.
+
+Example:
+
+```yaml
+mqtt_discoverystream:
+  base_topic: test_HA
+  publish_attributes: false
+  publish_timestamps: true
+  publish_discovery: true
+  include:
+    entities:
+      - sensor.owm_hourly_humidity
+      - sensor.jellyfin_cloud
+      - light.wled_esp
+  exclude:
+    entities:
+      - sensor.plug_xiaomi_1_electrical_measurement
+```
+
+## Configuration
+
+### Options
+
+This integration can only be configuration via YAML.
+The base options are the same as the mqtt_statestream one. 
+
+| key                | default | required | description                                                                |
+| ------------------ | ------- | -------- | -------------------------------------------------------------------------- |
+| base_topic         | none    | yes      | Base topic used to generate the actual topic used to publish.              |
+| publish_attributes | false   | no       | Publish attributes of the entity as well as the state.                     |
+| publish_timestamps | false   | no       | Publish the last_changed and last_updated timestamps for the entity.       |
+| publish_discovery  | false   | no       | Publish the discovery topic ("config").                                    |
+| include / exclude  | none    | no       | Configure which integrations should be included / exluded from publishing. |
+
+## Credits
+
+- This custom component is based upon the `mqtt_statestream` one from HA Core.  
---
+++ custom_components/mqtt_discoverystream/__init__.py
@@ -0,0 +1,243 @@
+"""Publish simple item state changes via MQTT."""
+import json
+import logging
+
+import voluptuous as vol
+
+from homeassistant.components.mqtt import valid_publish_topic
+from homeassistant.const import (
+    MATCH_ALL,
+    ATTR_ENTITY_ID,
+    SERVICE_TURN_OFF,
+    SERVICE_TURN_ON,
+    STATE_OFF,
+    STATE_ON,
+    CONF_INCLUDE,
+)
+from homeassistant.components.light import (
+    ATTR_BRIGHTNESS,
+    ATTR_COLOR_TEMP,
+    ATTR_RGB_COLOR,
+    ATTR_XY_COLOR,
+    ATTR_HS_COLOR,
+    SUPPORT_BRIGHTNESS,
+    SUPPORT_COLOR,
+    SUPPORT_COLOR_TEMP,
+    SUPPORT_EFFECT,
+)
+from homeassistant.core import callback
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity import get_supported_features
+from homeassistant.helpers.entityfilter import (
+    INCLUDE_EXCLUDE_BASE_FILTER_SCHEMA,
+    convert_include_exclude_filter,
+)
+from homeassistant.helpers.event import async_track_state_change
+from homeassistant.helpers.json import JSONEncoder
+
+_LOGGER = logging.getLogger(__name__)
+
+ATTR_COLOR = "color"
+ATTR_H = "h"
+ATTR_S = "s"
+ATTR_X = "x"
+ATTR_Y = "y"
+ATTR_R = "r"
+ATTR_G = "g"
+ATTR_B = "b"
+
+CONF_BASE_TOPIC = "base_topic"
+CONF_PUBLISH_ATTRIBUTES = "publish_attributes"
+CONF_PUBLISH_TIMESTAMPS = "publish_timestamps"
+CONF_PUBLISH_DISCOVERY = "publish_discovery"
+
+DOMAIN = "mqtt_discoverystream"
+
+CONFIG_SCHEMA = vol.Schema(
+    {
+        DOMAIN: INCLUDE_EXCLUDE_BASE_FILTER_SCHEMA.extend(
+            {
+                vol.Required(CONF_BASE_TOPIC): valid_publish_topic,
+                vol.Optional(CONF_PUBLISH_ATTRIBUTES, default=False): cv.boolean,
+                vol.Optional(CONF_PUBLISH_TIMESTAMPS, default=False): cv.boolean,
+                vol.Optional(CONF_PUBLISH_DISCOVERY, default=False): cv.boolean,
+            }
+        ),
+    },
+    extra=vol.ALLOW_EXTRA,
+)
+
+
+async def async_setup(hass, config):
+    """Set up the MQTT state feed."""
+    conf = config.get(DOMAIN)
+    publish_filter = convert_include_exclude_filter(conf)
+    has_includes = True if conf.get(CONF_INCLUDE) else False
+    base_topic = conf.get(CONF_BASE_TOPIC)
+    publish_attributes = conf.get(CONF_PUBLISH_ATTRIBUTES)
+    publish_timestamps = conf.get(CONF_PUBLISH_TIMESTAMPS)
+    publish_discovery = conf.get(CONF_PUBLISH_DISCOVERY)
+    if not base_topic.endswith("/"):
+        base_topic = f"{base_topic}/"
+    hass.data[DOMAIN] = {}
+    hass.data[DOMAIN][base_topic] = {}
+    hass.data[DOMAIN][base_topic]["conf_published"] = []
+
+
+    async def message_received(msg):
+        """Handle new messages on MQTT."""
+        explode_topic = msg.topic.split("/")
+        domain = explode_topic[1]
+        entity = explode_topic[2]
+        element = explode_topic[3]
+
+        _LOGGER.debug(f"Message received: topic {msg.topic}; payload: {msg.payload}")
+        if element == "set":
+            if msg.payload == STATE_ON:
+               await  hass.services.async_call(domain, SERVICE_TURN_ON, {ATTR_ENTITY_ID: f"{domain}.{entity}"})
+            elif msg.payload == STATE_OFF:
+               await hass.services.async_call(domain, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: f"{domain}.{entity}"})
+            else:
+                _LOGGER.error(f'Invalid service for "set" - payload: {msg.payload} for {entity}')
+        if element == "set_light":
+            if domain != "light":
+                _LOGGER.error(f'Invalid domain for "set_light" - payload: {msg.payload} for {entity}')
+            else:
+                payload_json = json.loads(msg.payload)
+                if payload_json["state"] == "ON":  
+                    service_payload =  {
+                        ATTR_ENTITY_ID: f"{domain}.{entity}",
+                    }
+                    if ATTR_BRIGHTNESS in payload_json:
+                        service_payload[ATTR_BRIGHTNESS] = payload_json[ATTR_BRIGHTNESS]
+                    if ATTR_COLOR in payload_json:
+                        if ATTR_H in payload_json[ATTR_COLOR]:
+                            service_payload[ATTR_HS_COLOR] = [ payload_json[ATTR_COLOR][ATTR_H], payload_json[ATTR_COLOR][ATTR_S] ]
+                        if ATTR_X in payload_json[ATTR_COLOR]:
+                            service_payload[ATTR_XY_COLOR] = [ payload_json[ATTR_COLOR][ATTR_X], payload_json[ATTR_COLOR][ATTR_Y] ]
+                        if ATTR_R in payload_json[ATTR_COLOR]:
+                            service_payload[ATTR_RGB_COLOR] = [ payload_json[ATTR_COLOR][ATTR_R], payload_json[ATTR_COLOR][ATTR_G], payload_json[ATTR_COLOR][ATTR_B] ]
+                    await hass.services.async_call(domain, SERVICE_TURN_ON, service_payload)
+                elif payload_json["state"] == "OFF":
+                    await hass.services.async_call(domain, SERVICE_TURN_OFF, {ATTR_ENTITY_ID: f"{domain}.{entity}"})
+                else:
+                    _LOGGER.error(f'Invalid state for "set_light" - payload: {msg.payload} for {entity}')
+
+
+    @callback
+    def _state_publisher(entity_id, old_state, new_state):
+        if new_state is None:
+            return
+
+        _LOGGER.info(f'Checking {entity_id}')
+
+        if not publish_filter(entity_id):
+            return
+
+        _LOGGER.info(f'Handling {entity_id}')
+        mybase = f"{base_topic}{entity_id.replace('.', '/')}/"
+
+        if publish_timestamps:
+            if new_state.last_updated:
+                hass.components.mqtt.async_publish(
+                    f"{mybase}last_updated", new_state.last_updated.isoformat(), 1, True
+                )
+            if new_state.last_changed:
+                hass.components.mqtt.async_publish(
+                    f"{mybase}last_changed", new_state.last_changed.isoformat(), 1, True
+                )
+
+        if publish_attributes:
+            for key, val in new_state.attributes.items():
+                encoded_val = json.dumps(val, cls=JSONEncoder)
+                hass.components.mqtt.async_publish(mybase + key, encoded_val, 1, True)
+
+        ent_parts = entity_id.split(".") 
+        ent_domain = ent_parts[0]
+        ent_id = ent_parts[1]
+
+        if publish_discovery and not entity_id in hass.data[DOMAIN][base_topic]["conf_published"]:
+            config = {
+                "uniq_id": f"mqtt_{entity_id}",
+                "name": ent_id.replace("_", " ") .title(),
+                "stat_t": f"{mybase}state",
+                "json_attr_t": f"{mybase}attributes"
+            }
+            if ("device_class" in new_state.attributes):
+                config["dev_cla"] = new_state.attributes["device_class"]
+            if ("unit_of_measurement" in new_state.attributes):
+                config["unit_of_meas"] = new_state.attributes["unit_of_measurement"]
+            if ("state_class" in new_state.attributes):
+                config["stat_cla"] = new_state.attributes["state_class"]
+            
+
+            publish_config = False
+            if ent_domain == "sensor" and (has_includes or "device_class" in new_state.attributes):
+                publish_config = True
+            elif ent_domain == "binary_sensor" and (has_includes or "device_class" in new_state.attributes):
+                publish_config = True
+            elif ent_domain == "switch":
+                config["pl_off"] = STATE_OFF
+                config["pl_on"] = STATE_ON
+                config["cmd_t"] = f"{mybase}set"
+
+                publish_config = True
+            elif ent_domain == "light":
+                config = {
+                    "uniq_id": f"mqtt_{entity_id}",
+                    "name": ent_id.replace("_", " ") .title(),
+                    "stat_t": f"{mybase}state",
+                    "cmd_t": f"{mybase}set_light",
+                    "schema": "json"
+                }
+                supported_features = get_supported_features(hass, entity_id)
+                if supported_features & SUPPORT_BRIGHTNESS:
+                    config["brightness"] = True
+                if supported_features & SUPPORT_EFFECT:
+                    config["effect"] = True
+                if "supported_color_modes" in new_state.attributes:
+                    config["color_mode"] = True
+                    config["supported_color_modes"] = new_state.attributes["supported_color_modes"]
+
+                publish_config = True
+
+            if publish_config:
+                encoded = json.dumps(config, cls=JSONEncoder)
+                hass.components.mqtt.async_publish(f"{mybase}config", encoded, 1, True)
+                hass.data[DOMAIN][base_topic]["conf_published"].append(entity_id)
+
+        if publish_discovery:
+            if ent_domain == "light":
+                payload = {
+                    "state": "ON" if new_state.state == STATE_ON else "OFF",
+                }
+                if ("brightness" in new_state.attributes):
+                    payload["brightness"] = new_state.attributes["brightness"]
+                if ("color_mode" in new_state.attributes):
+                    payload["color_mode"] = new_state.attributes["color_mode"]
+                if ("color_temp" in new_state.attributes):
+                    payload["color_temp"] = new_state.attributes["color_temp"]
+                if ("effect" in new_state.attributes):
+                    payload["effect"] = new_state.attributes["effect"]
+                
+                hass.components.mqtt.async_publish(f"{mybase}state", json.dumps(payload, cls=JSONEncoder), 1, True)
+            else:
+                payload = new_state.state
+                hass.components.mqtt.async_publish(f"{mybase}state", payload, 1, True)
+
+                attributes = {}
+                for key, val in new_state.attributes.items():
+                    attributes[key] = val
+                encoded = json.dumps(attributes, cls=JSONEncoder)
+                hass.components.mqtt.async_publish(f"{mybase}attributes", encoded, 1, True)
+        else:
+            payload = new_state.state
+            hass.components.mqtt.async_publish(f"{mybase}state", payload, 1, True)
+
+
+    if publish_discovery:
+        await hass.components.mqtt.async_subscribe(f"{base_topic}switch/+/set", message_received)
+        await hass.components.mqtt.async_subscribe(f"{base_topic}light/+/set_light", message_received)
+
+    async_track_state_change(hass, MATCH_ALL, _state_publisher)
+    return True
